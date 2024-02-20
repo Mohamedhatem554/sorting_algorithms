f35733dc0fe2901f36ec7e0f34728916f027615e
Mohamed <Mohamed@me>
2024-02-21 00:27:22 +0200

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,25 @@
 #include "sort.h"
 
+/**
+ * swap - swap function
+ * @first: first number
+ * @second: second number
+ * Return: Nothing
+*/
+void swap(listint_t *first, listint_t *second)
+{
+	if (first->prev)
+		first->prev->next = second;
+
+	if (second->next)
+		second->next->prev = first;
+
+	first->next = second->next;
+	second->prev = first->prev;
+	first->prev = second;
+	second->next = first;
+}
+
 
 /**
  * insertion_sort_list - insertion sort
